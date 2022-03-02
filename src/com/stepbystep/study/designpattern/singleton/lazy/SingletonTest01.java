b9d7236eb33885ea8e6a67a26bdfb7e5e41db7f0
forbearcounterattack <hhxxttxs202012@126.com>
2022-03-02 17:49:24 +0800

    add singleton test

---
+++ src/com/stepbystep/study/designpattern/singleton/lazy/SingletonTest01.java
@@ -0,0 +1,11 @@
+package com.stepbystep.study.designpattern.singleton.lazy;
+
+/**
+ * @Author Duole
+ * @Version 1.0
+ */
+public class SingletonTest01 {
+    public static void main(String[] args) {
+        
+    }
+}
